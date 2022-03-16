a3792d8cc8ae50b93272a1fc3f45f32964f61483
TasukuY <alohaty81@gmail.com>
2022-03-16 10:22:00 -0600

    added another solution

--- romanNumerals.js
+++ romanNumerals.js
@@ -28,18 +28,40 @@ let makeRomanNumeral = num => {
     return romanNumeral
 }
 
-// let makeRomanNumeral2 = num => {
-//     let hundreds = ['C', 'CC', 'CCC', 'CCCC', 'D', 'DC', 'DCC', 'DCCC', 'DCCCC'];
-//     let tens = ['X', 'XX', 'XXX', 'XXXX', 'L', 'LX', 'LXX', 'LXXX', 'LXXXX'];
-//     let ones = ['I', 'II', 'III', 'IIII', 'V', 'VI', 'VII', 'VIII', 'VIIII'];
-//     let firstPlace = num % 10;
-//     let tenthPlace = (num - firstPlace) % 100;
-//     let hundredthPlace = (num - (firstPlace + tenthPlace)) % 1000;
+let makeRomanNumeral2 = num => {
+    if(num === 1000){
+        return 'M';
+    }
 
+    let romanNum = [];
+    let hundreds = ['C', 'CC', 'CCC', 'CCCC', 'D', 'DC', 'DCC', 'DCCC', 'DCCCC'];
+    let tens = ['X', 'XX', 'XXX', 'XXXX', 'L', 'LX', 'LXX', 'LXXX', 'LXXXX'];
+    let ones = ['I', 'II', 'III', 'IIII', 'V', 'VI', 'VII', 'VIII', 'VIIII'];
+    let numPlaceValue = placeValue(num);
+    let counter = 1;
 
+    for(let i = numPlaceValue.length - 1; i >= 0; i--){
+        let index = (numPlaceValue[i] / counter) - 1;
+        if(counter === 1){
+            romanNum.unshift(ones[index]);
+        }else if(counter === 10){
+            romanNum.unshift(tens[index]);
+        }else if(counter === 100){
+            romanNum.unshift(hundreds[index]);
+        }
+        counter *= 10;
+    }
+    return romanNum.join('');
+}
 
-// }
+const placeValue = (num, res = [], factor = 1) => {
+   if(num){
+      const val = (num % 10) * factor;
+      res.unshift(val);
+      return placeValue(Math.floor(num / 10), res, factor * 10);
+   };
+   return res;
+};
 
-console.log(makeRomanNumeral(242));
-
-// console.log();
+console.log(makeRomanNumeral(1000));
+console.log(makeRomanNumeral2(1000));
